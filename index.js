144546c829670da11d2b6ba5679d2ae22b9b9a42
Lily2point0 <lily.madar@ft.com>
2020-01-27 15:09:51 +0000

    cleanup

--- index.js
+++ index.js
@@ -43,12 +43,8 @@ app.post('/add', (req,res) => {
 
 app.post('/submit', async (req, res) => {
 	const response = JSON.parse(req.body.payload);
-	// console.log('Submit', JSON.stringify(response));
 	const actionType = response.type;
 	let viewID;
-
-	// const blockID = response.actions[0].block_id;
-	// console.log('it\'s still going to submit');
 
 	//TODO: handle validation for all actions
 
@@ -78,42 +74,9 @@ app.post('/submit', async (req, res) => {
 			viewID = response.container.view_id;
 			const property = response.actions[0].action_id;
 			Input.add(viewID, property, response.actions[0].selected_option.value);
-			console.log(response.actions);
 			return res.sendStatus(200);
 		break;
-// 		case 'button':
-// 			const user = response.user.name;
-// 			const submission = Input.submit(blockID, user);
-
-// 			if(submission) {
-// 				await Sheet.write(submission, () => {
-// 					Input.deleteBlock(blockID);
-
-// 					//TODO: replace the text/collapse block;
-// 					return res.send('New row added');
-// 				});
-// 			} else {
-// 				//TODO: send/display error
-// 				console.log('No block to submit');
-// 				return res.sendStatus(200);	
-// 			}
-			
-// 		break;
-
-// 		case 'static_select':
-// 			const property = response.actions[0].action_id;
-// 			Input.add(blockID, property, response.actions[0].selected_option.value);
-			
-// 			return res.sendStatus(200);
-// 		break;
-
-// 		case 'datepicker':
-// 			Input.add(blockID, 'releaseDate', response.actions[0].selected_date);
-// 			return res.sendStatus(200);
-// 		break;
 	}
-
-	console.log('VIEW', viewID);
 });
 
 async function setUpStructure() {
